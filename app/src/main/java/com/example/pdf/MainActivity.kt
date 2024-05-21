455d77a846583baa1ea2d29b7b968e2c7a52d222
Sukriti <sonal147sukriti@gmail.com>
2024-05-22 02:54:29 +0530

    Finished Project for Pdf Reader

---
+++ app/src/main/java/com/example/pdf/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.pdf
+
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pdf.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+                launcher.launch("application/pdf")
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ){uri ->
+
+        uri?.let {
+            binding.pdfView.fromUri(it).load()
+        }
+    }
+}
